c42294b38187426297a2502e764414d862ca191b
Jan Kuchař <honza.kuchar@grifart.cz>
2017-10-31 18:53:07 +0100

    Quick&dirty checking of annotations and defined instances

--- src/Enum.php
+++ src/Enum.php
@@ -9,7 +9,11 @@ abstract class Enum
 
 	abstract public static function provideInstances(): array;
 
-	public static function getValues(): array
+	// Primitive values
+	/**
+	 * @return string[]|int[]
+	 */
+	public static function getPrimitiveValues(): array
 	{
 		$classReflection = new \ReflectionClass(static::class);
 
@@ -17,25 +21,129 @@ abstract class Enum
 		return $classReflection->getConstants();
 	}
 
-	public static function getValue(string $key): string
+	public static function getPrimitiveValueForConstantName(string $key): string
 	{
 		// todo: error handling
-		return static::getValues()[$key];
+		return static::getPrimitiveValues()[$key];
 	}
 
-	/** @var array|null */
-	protected static $instances;
+	private static function getConstantsNames(): array
+	{
+		return array_keys(self::getPrimitiveValues());
+	}
 
-	public static function __callStatic(string $name, array $arguments)
+	// Enum instances
+
+	/** @var array|null */
+	protected static $instances = [];
+
+	public static function __callStatic(string $constantName, array $arguments)
 	{
 		\assert(empty($arguments));
 
-		$value = static::getValue($name);
-		if (static::$instances === NULL) {
-			static::$instances = static::provideInstances();
-		}
-		assert(isset(static::$instances[$value])); // FIXME
-		assert(static::$instances[$value] instanceof static);
-		return static::$instances[$value];
+		/** @var string|\Grifart\Enum\Enum $constantName */
+		return static::getValueForPrimitive(
+			self::getPrimitiveValueForConstantName($constantName)
+		);
 	}
+
+	private static function getInstancesForClass(string $forClass)
+	{
+		if (!isset(static::$instances[$forClass])) {
+			static::$instances[$forClass] = static::provideInstances();
+			self::checkAnnotations();
+		}
+		return static::$instances[$forClass];
+	}
+
+	/**
+	 * @param string $primitive
+	 * @return static
+	 */
+	public static function getValueForPrimitive(string $primitive) {
+		$instances = self::getInstancesForClass(static::class);
+
+		assert(isset($instances[$primitive])); // FIXME: better message
+		assert($instances[$primitive] instanceof static);
+		return $instances[$primitive];
+	}
+
+
+
+	private static function getConstantNameForPrimitiveValue($primitiveValue): string
+	{
+		return \array_search($primitiveValue, static::getPrimitiveValues(), TRUE);
+	}
+
+	/**
+	 * @return int|string
+	 */
+	public function getPrimitiveValue()
+	{
+		return array_search($this, self::getInstancesForClass(static::class), TRUE);
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	// Checking of definition consistency
+
+	private static function checkAnnotations(): void
+	{
+		$enumReflection = new \ReflectionClass(static::class);
+
+		self::checkCallStaticAnnotations($enumReflection);
+		self::checkAllInstancesProvided($enumReflection->getName());
+	}
+
+	private static function checkCallStaticAnnotations(\ReflectionClass $enumReflection): void
+	{
+		$docBlock = $enumReflection->getDocComment();
+		$className = $enumReflection->getShortName();
+		if ($docBlock === FALSE) {
+			$docBlock = '';
+		}
+
+		$missingAnnotations = [];
+		foreach (static::getConstantsNames() as $constantName) {
+			$desiredAnnotation = "@method static $className $constantName()";
+			if (stripos($docBlock, $desiredAnnotation) === false) {
+				$missingAnnotations[] = $desiredAnnotation;
+			}
+		}
+
+		if(!empty($missingAnnotations)) {
+			$properDoc = "/**\n * " . implode("\n * ", $missingAnnotations) . "\n */\n";
+			throw new \LogicException("You forgotten to add @method annotations for enum {$enumReflection->getName()}. Missing ones are: \n$properDoc");
+		}
+
+		// todo: @method annotations without constants
+	}
+
+	private static function checkAllInstancesProvided(string $className): void
+	{
+		// todo: instances without constants
+		$instances = self::getInstancesForClass($className);
+		foreach(static::getPrimitiveValues() as $primitiveValue) {
+			$constantName = static::getConstantNameForPrimitiveValue($primitiveValue);
+			if (!isset($instances[$primitiveValue])) {
+				throw new \LogicException("You have forgotten to provide instance for $constantName.");
+			}
+		}
+	}
+
+
 }
