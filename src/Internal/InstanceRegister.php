5a86003f6fdb95d1a00fdbdf94b6013b74262617
Jan Kuchař <honza.kuchar@grifart.cz>
2019-01-09 22:58:39 +0100

    InstanceRegister: more comments removed unused code

--- src/Internal/InstanceRegister.php
+++ src/Internal/InstanceRegister.php
@@ -2,22 +2,25 @@
 
 namespace Grifart\Enum\Internal;
 
+/**
+ * Keeps track of all enum instances organized by enum root classes.
+ */
 final class InstanceRegister
 {
 	/** @var \Grifart\Enum\Internal\Meta[] */
 	private static $instances = [];
 
-	public static function get(string $enumClass, callable $registrator = null): Meta
+	public static function get(string $enumClass, callable $registrar): Meta
 	{
-		if (!isset(self::$instances[$enumClass]) && $registrator !== null) {
-			self::register($registrator());
+		if (!isset(self::$instances[$enumClass])) {
+			self::register($registrar());
 		}
 		return self::$instances[$enumClass];
 	}
 
 	public static function register(Meta $meta): void
 	{
-		// check consistency of enum when assertions are enabled
+		// check consistency of enum when assertions are enabled (typically non-production code)
 		assert(
 			(function () use ($meta): bool {
 				ConsistencyChecker::checkAnnotations($meta);
